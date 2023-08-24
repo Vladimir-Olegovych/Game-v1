c1839220dbd9a633a20a8a83ad3be605549efb38
Vladimir <117392112+Vladimir-Olegovych@users.noreply.github.com>
2023-08-24 18:50:24 +0300

    second commit

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -3,11 +3,21 @@
 #endif 
 
 #include <windows.h>
+#include <gdiplus.h>
 
+void draw(HDC hdc);
+
+//uint_16 max 65535
+const UINT16 height = 500;
+const UINT16 width = 1000;
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-
+    //GDI+
+    Gdiplus::GdiplusStartupInput gdiPlusInput;
+    ULONG_PTR gdiPlusToken;
+    Gdiplus::GdiplusStartup(&gdiPlusToken, &gdiPlusInput, nullptr);
+    //Screen
     const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
     WNDCLASSEX wc = { };
@@ -20,6 +30,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)S_WHITE2048;
     wc.hInstance = hInstance;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
@@ -32,13 +43,13 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     hwnd = CreateWindowEx(
         0,                         // Optional window styles.
         CLASS_NAME,                // Window class
-        L"(Кто прочитал тот гей)",   // Window text
+        L"(Math Test)",   // Window text
         WS_OVERLAPPEDWINDOW,       // Window style
 
         CW_USEDEFAULT,  //0
         CW_USEDEFAULT,  //0
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        width,
+        height,
 
         NULL,       // Parent window    
         NULL,       // Menu
@@ -56,27 +67,38 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+    Gdiplus::GdiplusShutdown(gdiPlusToken);
 
     return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
+    HDC hdc;
+    PAINTSTRUCT ps;
+
     switch (uMsg) {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
 
-    case WM_PAINT: {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
+        case WM_PAINT: {
+            hdc = BeginPaint(hwnd, &ps);
+            //Render
+            draw(hdc);
 
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-        EndPaint(hwnd, &ps);
-    }
-                 return 0;
+            return 0;
+        }
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+                     return 0;
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+void draw(HDC hdc) {
+    boolean ads = true;
+    Gdiplus::Graphics gf(hdc);
+    Gdiplus::Pen pen(Gdiplus::Color(255, 255, 0, 0));
+    for (int i = 0; i < 1000; i++) {
+        gf.DrawLine(&pen, i, 500, i, rand() % height);
+    }
 }
