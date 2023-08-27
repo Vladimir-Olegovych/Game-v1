dc53938c3f0a66a4143e305cfd32ed778a09a939
Vladimir <117392112+Vladimir-Olegovych@users.noreply.github.com>
2023-08-27 14:34:36 +0300

    sixth commit

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -16,22 +16,24 @@ struct Snake {
 
     UINT16 i = 0;
 
-    short x = 50;
-    short y = 50;
+    short x = 0;
+    short y = 0;
 };
 struct Apple {
     boolean spawned = true;
-    UINT16 size = 10;
+    UINT16 size = 0;
     short x = 0;
     short y = 0;
 };
 //functions
 void drawSnake(HDC hdc);
 void drawApple(HDC hdc);
+void update();
 //const
-const UINT16 height = GetSystemMetrics(SM_CYSCREEN)-40;
+const UINT16 height = GetSystemMetrics(SM_CYSCREEN)-30;
 const UINT16 width = GetSystemMetrics(SM_CXSCREEN);
-const UINT8 speed = 26;
+const UINT8 snakeSize = 70;
+const UINT8 speed = snakeSize/2;
 //snake logic
 Snake snake;
 Apple apple;
@@ -122,9 +124,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 break;
             }
             //Space
-            if (wParam == 0x20) {
+            if (wParam == 0x20) { 
                 apple.spawned = true;
-                RedrawWindow(hwnd, NULL, NULL, uMsg);
                 break;
             }
             //Tab
@@ -135,37 +136,35 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             //W
             if (wParam == 0x57) {
                 y -= speed;
-                if (y <= -50) {y = height;}
+                if (y <= -50) {y = height-y;}
                 RedrawWindow(hwnd, NULL, NULL, uMsg);
                 break;
             }
             //S
             if (wParam == 0x53) {
                 y += speed;
-                if (y >= height) {y = -50;}
+                if (y >= height) {y = height-y;}
                 RedrawWindow(hwnd, NULL, NULL, uMsg);     
                 break;
             }
             //A
             if (wParam == 0x41) {
                 x -= speed;
-                if (x <= -50) {x = width;}
+                if (x <= -50) {x = width-x;}
                 RedrawWindow(hwnd, NULL, NULL, uMsg);
                 break;
             }
             //D
             if (wParam == 0x44) {
                 x += speed;
-                if (x >= width) {x = -50;}
+                if (x >= width) {x = width-x;}
                 RedrawWindow(hwnd, NULL, NULL, uMsg);
                 break;
             }
         }
         case WM_PAINT: {
             //Update
-            snake.x = x;
-            snake.y = y;
-            arr.push_back(snake);
+            update();
             //Render
             hdc = BeginPaint(hwnd, &ps);
 
@@ -179,24 +178,34 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
+void update() {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i].x != -1 && arr[i].x == x && arr[i].y == y)
+        {
+            exit(0);
+        }
+    }
+    snake.x = x;
+    snake.y = y;
+    arr.push_back(snake);
+}
 void drawApple(HDC hdc) {
     Graphics gf(hdc);
-    SolidBrush brush(Color(255, 0, 255, 0));
+    SolidBrush brush(Color::LightBlue);
 
     apple.spawned = false;
-
     random_device rd;
-    uniform_int_distribution<short> distX(1, width-200);
-    uniform_int_distribution<short> distY(1, height-200);
+    uniform_int_distribution<short> distX(1, width-150);
+    uniform_int_distribution<short> distY(1, height-150);
     uniform_int_distribution<short> distA(5, 110);
 
     apple.size = distA(rd);
     apple.x = distX(rd);
     apple.y = distY(rd);
 
-    if (apple.size <= 15) brush.SetColor(Color(255, 255, 0, 0));
-    if (apple.size > 50 && apple.size < 100) brush.SetColor(Color(255, 0, 255, 0));
-    if (apple.size >= 100) brush.SetColor(Color(255, 0, 255, 255));
+    if (apple.size <= 15) brush.SetColor(Color::Red);
+    if (apple.size > 50 && apple.size < 100) brush.SetColor(Color::White);
+    if (apple.size >= 100) brush.SetColor(Color::Blue);
 
 
     gf.FillEllipse(&brush, apple.x, apple.y, apple.size, apple.size);
@@ -204,14 +213,28 @@ void drawApple(HDC hdc) {
 
 void drawSnake(HDC hdc) {
     Graphics gf(hdc);
-    SolidBrush brush(Color(100, 255, 0, 0));
-    SolidBrush delBrush(Color(150, 10, 0, 0));
+    SolidBrush brush(Color::White);
+    SolidBrush delBrush(Color::Black);
 
+    random_device rd;
+    uniform_int_distribution<short> colR(1, 7);
     //SnakeLogic
-    gf.FillRectangle(&brush, x, y, 50, 50);
+    switch (colR(rd)) {
+        case 1: brush.SetColor(Color::Red); break;
+        case 2: brush.SetColor(Color::Orange); break;
+        case 3: brush.SetColor(Color::Yellow); break;
+        case 4: brush.SetColor(Color::Green); break;
+        case 5: brush.SetColor(Color::LightBlue); break;
+        case 6: brush.SetColor(Color::Blue); break;
+        case 7: brush.SetColor(Color::Purple); break;
+    }
+    gf.FillRectangle(&brush, x, y, snakeSize, snakeSize);
     if (snake.maxEnumSnake >= snake.i){ snake.i++; }
     else {
-        gf.FillRectangle(&delBrush, arr[snake.enumSnake].x, arr[snake.enumSnake].y, 50, 50);
+        gf.FillRectangle(&delBrush, arr[snake.enumSnake].x, arr[snake.enumSnake].y, snakeSize, snakeSize);
+        snake.x = -1;
+        snake.y = -1;
+        arr[snake.enumSnake] = snake;
         snake.enumSnake++;
     }
 
